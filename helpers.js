3838e85084a4a38e6f471199c27c91e6e12aaae9
lizlove <lizlovero@havenlife.com>
2018-01-22 09:21:44 -0500

    Basic styling for red theme.

--- helpers.js
+++ helpers.js
@@ -11,9 +11,6 @@ exports.moment = require('moment');
 // Dump is a handy debugging function we can use to sort of "console.log" our data
 exports.dump = (obj) => JSON.stringify(obj, null, 2);
 
-// Making a static map is really long - this is a handy helper function to make one
-exports.staticMap = ([lng, lat]) => `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=800x150&key=${process.env.MAP_KEY}&markers=${lat},${lng}&scale=2`;
-
 // inserting an SVG
 exports.icon = (name) => fs.readFileSync(`./public/images/icons/${name}.svg`);
 
@@ -23,6 +20,6 @@ exports.siteName = `Ok, not ok`;
 exports.menu = [
   { slug: '/stories', title: 'Stories', icon: 'store', },
   { slug: '/results', title: 'Results', icon: 'tag', },
-  { slug: '/add', title: 'Add', icon: 'add', },
-  { slug: '/about', title: 'About', icon: 'top', }
+  { slug: '/submit', title: 'Submit', icon: 'submit', },
+  { slug: '/about', title: 'About', icon: 'about', }
 ];
