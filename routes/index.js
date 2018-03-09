338f5009b359ce3957a45fa1fc545b61b29c0722
lizlove <elizabethlovero@gmail.com>
2018-03-09 09:06:29 -0500

    Rebase models.

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,11 @@ const { catchErrors } = require('../handlers/errorHandlers');
 // Routing around
 router.get('/', catchErrors(storyController.getStories));
 router.get('/stories', catchErrors(storyController.getStories));
+<<<<<<< HEAD
 router.get('/add', storyController.addStory);
 router.post('/add', catchErrors(storyController.createStory)); //wraps async+await mongoose function in higher order error handling function
+=======
+router.get('/stories/page/:page', catchErrors(storyController.getStories));
+>>>>>>> Pagination.
 
 module.exports = router;
