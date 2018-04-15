9fbbe1c29839c7580d13165b7d5255b1dd57a162
lizlove <elizabethlovero@gmail.com>
2018-04-15 17:02:08 -0400

    Send rating name and story name.

--- controllers/ratingController.js
+++ controllers/ratingController.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const Rating = mongoose.model('Rating');
 
 exports.addRating = async (req, res) => {
+  console.log('RETURNED', req);
   // 1. Get author string from session id.
   req.body.author = req.user._id;
   // 2. Set store id
-  console.log('RETURNED', req.body);
   req.body.story = req.params.id;
   // 3. Set rating
   req.body.rating = req.query.rating;
